ee177f66c295a7c67085da1948cb782be66ae3fe
wangyinuo1111111 <15236647097@163.com>
2020-03-11 11:45:52 +0800

    测试git是否可用

---
+++ src/main/java/com/neo/entity/Bb.java
@@ -0,0 +1,31 @@
+package com.neo.entity;
+
+import java.util.Map;
+
+/**
+ * @Auther: ynwang
+ * @Date: 2020/1/20
+ * @Description:
+ */
+public class Bb {
+
+    private String bb;
+
+    private Map<String,Object> args;
+
+    public String getBb() {
+        return bb;
+    }
+
+    public void setBb(String bb) {
+        this.bb = bb;
+    }
+
+    public Map<String,Object> getArgs() {
+        return args;
+    }
+
+    public void setArgs(Map<String,Object> args) {
+        this.args = args;
+    }
+}
--- src/main/java/com/neo/web/test/PageController.java
+++ src/main/java/com/neo/web/test/PageController.java
@@ -1,11 +1,17 @@
 package com.neo.web.test;
 
+import com.alibaba.fastjson.JSONObject;
+import com.neo.entity.Bb;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class PageController {
@@ -22,4 +28,19 @@ public class PageController {
         System.out.println(reqData);
         return "a";
     }
+
+    @RequestMapping(value = "/test1",method = RequestMethod.POST,consumes = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String efficientFrontier(@RequestBody Bb bb){
+        System.out.println(bb);
+        return "bb";
+    }
+
+    public static void main(String[] args) {
+        Map<String,Object> map = new HashMap<>();
+        map.put("aaa","aaa");
+        map.put("aaa1","aaa1");
+        System.out.println(JSONObject.toJSONString(map));
+    }
+
 }
